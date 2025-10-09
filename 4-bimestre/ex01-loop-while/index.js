6cff41c45f278ac9d90c88281def2bc32850a97e
stefany <stefany.ribeiro4@aluno.ce.gov.br>
2025-10-09 17:49:29 +0000

    feito

--- 4-bimestre/ex01-loop-while/index.js
+++ 4-bimestre/ex01-loop-while/index.js
@@ -1,4 +1,18 @@
 // CRIE UMA LÓGICA AQUI
+let resultado = 1
+
+while (resultado < 10) {
+    console.log (`${resultado}`)
+    resultado++
+}
+
+
+
+
+
+
+
+
 
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
